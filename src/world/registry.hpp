b4d9fa07e6822b806d86328707da78d5f03535db
Mathijs Follon <mathijs.follon@gmail.com>
2026-04-09 13:32:57 +0200

    Add some docs

--- src/world/registry.hpp
+++ src/world/registry.hpp
@@ -15,14 +15,14 @@ class Registry {
 public:
     Registry() = default;
 
-    template <typename T>
-    void registerObject(std::string name, T object) {
+    void registerObject(std::string name, auto object) {
         m_objects[std::move(name)] = std::move(object);
     }
 
     template <typename T>
     [[nodiscard]] bool hasObject(const std::string& name) const {
         if (const auto it = m_objects.find(name); it != m_objects.end()) {
+            // If found an object with name, return true if the found type matches the requested type
             return it->second.type() == typeid(T);
         }
         return false;
@@ -31,6 +31,7 @@ public:
     template <typename T>
     [[nodiscard]] T* getObject(const std::string& name) {
         if (const auto it = m_objects.find(name); it != m_objects.end()) {
+            // Cast the std::any type to the requested type
             return std::any_cast<T>(&it->second);
         }
         return nullptr;
@@ -47,16 +48,18 @@ public:
     template <typename T>
     auto getObjects() {
         return m_objects | std::views::values |
+                // Cast all to the requested T type
                std::views::transform([](std::any& value) { return std::any_cast<T>(&value); }) |
+                // Filter out failed casts (nullptr's)
                std::views::filter([](const T* value) { return value != nullptr; });
     }
 
+
     template <typename T>
     auto getObjects() const {
-        return m_objects | std::views::values | std::views::transform([](const std::any& value) {
-                   return std::any_cast<const T>(&value);
-               }) |
-               std::views::filter([](const T* value) { return value != nullptr; });
+        return m_objects | std::views::values |
+            std::views::transform([](const std::any& value) { return std::any_cast<const T>(&value); }) |
+            std::views::filter([](const T* value) { return value != nullptr; });
     }
 
     void emitEvent(const Event event) { m_events.push_back(event); }
@@ -65,17 +68,22 @@ public:
     void emitEvent(EventPayload payload) {
         static_assert(std::is_enum_v<decltype(EventTraits<EventPayload>::type)>,
                       "EventTraits must define a valid EventType");
+        // Uses traits to define the EventType of a certain EventPayload.
+        // Eg. EventTraits<ApplicationExitRequest>::type == EventType::ApplicationExitRequest
         emitEvent(Event{EventTraits<EventPayload>::type, std::move(payload)});
     }
 
     [[nodiscard]] bool hasEvent(const EventType type) const {
-        return std::ranges::any_of(m_events,
-                                   [type](const Event& event) { return event.type == type; });
+        // Check if any event.type in range equals type
+        return std::ranges::any_of(m_events, [type](const Event& event) { return event.type == type; });
     }
 
     std::optional<Event> pollEvents(EventType type) {
+        // Find the first matching event on type
         const auto it = std::ranges::find_if(
             m_events, [type](const Event& event) { return event.type == type; });
+
+        // If there was a match then return and erase it
         if (it != m_events.end()) {
             Event event = *it;
             m_events.erase(it);
