9e6ca250d597fc7feaff43036a1711c8d4759728
Tarudahat <taruzahat@gmail.com>
2026-04-21 22:00:57 +0200

    Add Texture Loader

--- src/asset/texture.hpp
+++ src/asset/texture.hpp
@@ -2,6 +2,10 @@
 #define CG_OPENGL_PROJECT_TEXTURE_HPP
 
 #include <glad/gl.h>
+#include "log/log.hpp"
+
+#define STB_IMAGE_IMPLEMENTATION
+#include "stb_image.h"
 
 #include <optional>
 #include <string_view>
@@ -9,10 +13,48 @@
 namespace asset {
 
 struct Texture {
+    GLuint textureHdl;
 };
 
 [[nodiscard]] inline std::optional<Texture> loadTextureFromPath(std::string_view path) {
-    return std::nullopt;
+    stbi_set_flip_vertically_on_load(true);  // flip textures straight
+
+    // load image
+    int w, h, channelCnt;
+    unsigned char* textureData = stbi_load(path.data(), &w, &h, &channelCnt, 0);
+
+    if (textureData == NULL) {
+        LOG_ERROR("Failed to load image: {} because: {}", path, stbi_failure_reason());
+        return std::nullopt;
+    }
+
+    // generate texture object
+    GLuint textureHdl;
+    glGenTextures(1, &textureHdl);
+    glBindTexture(GL_TEXTURE_2D, textureHdl);
+
+    // setup texture
+    int inputFormat = GL_RGB;
+
+    if (channelCnt < 3) {
+        LOG_ERROR("Unsupported channel count in image");
+        stbi_image_free(textureData);
+        return std::nullopt;
+    } else if (channelCnt == 4)
+        inputFormat = GL_RGBA;
+
+    glTexImage2D(GL_TEXTURE_2D, 0, GL_RGB, w, h, 0, inputFormat, GL_UNSIGNED_BYTE, textureData);
+    glGenerateMipmap(GL_TEXTURE_2D);
+
+    // setup texture filtering
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MIN_FILTER, GL_LINEAR_MIPMAP_LINEAR);
+    glTexParameteri(GL_TEXTURE_2D, GL_TEXTURE_MAG_FILTER, GL_LINEAR);
+
+    // free loaded image (not texture) and unbind texture
+    stbi_image_free(textureData);
+    glBindTexture(GL_TEXTURE_2D, 0);
+
+    return Texture{textureHdl};
 }
 
 }  // namespace asset
